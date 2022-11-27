e60d4d7f9df0ed05817bf20d03129232803c4672
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-11-27 12:12:26 +0600

    Tests added

---
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -0,0 +1,56 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+    @Test
+    public void addFilmsAndFindAllTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film1");
+        manager.addMovie("Film2");
+        manager.addMovie("Film3");
+
+        String[] actual = manager.findAll();
+        String[] expected = {"Film1", "Film2", "Film3"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void revertedResultTestLessLimit() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film1");
+        manager.addMovie("Film2");
+        manager.addMovie("Film3");
+
+        String[] actual = manager.findLast(10);
+        String[] expected = {"Film3", "Film2", "Film1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void revertedResultTestEqualsLimit() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film1");
+        manager.addMovie("Film2");
+        manager.addMovie("Film3");
+
+        String[] actual = manager.findLast(3);
+        String[] expected = {"Film3", "Film2", "Film1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void revertedResultTestExceedsLimit() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film1");
+        manager.addMovie("Film2");
+        manager.addMovie("Film3");
+        manager.addMovie("Film4");
+
+        String[] actual = manager.findLast(3);
+        String[] expected = {"Film4", "Film3", "Film2"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
